bdeb94d444e28cd34fc24db2a03e767447e6eec1
Luis Eduardo Bolivar Cardenas <theluisbolivar@Luiss-MacBook-Pro.local>
2021-12-28 20:51:58 -0600

    add trim to validate character

--- src/com/co/pa/Main.java
+++ src/com/co/pa/Main.java
@@ -20,10 +20,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
-        System.out.println(isValidParentheses(line.trim()));
+        System.out.println(isValid(line));
     }
 
-    public static boolean isValidParentheses(String line){
+    public static boolean isValid(String line) {
+        line = line.trim();
         if (Boolean.TRUE.equals(firstValidation(line))) return false;
         for(int i = 0; i < line.length(); i++) validateChar(line.charAt(i));
         return parentheses.isEmpty() && squareBrackets.isEmpty() && braces.isEmpty();
@@ -37,11 +38,11 @@ public class Main {
 
     public static boolean startWithClose(String line){
         char start = line.charAt(0);
-        return start == CLOSE_PARENTHESES || start == CLOSE_BRACES || start == CLOSE_SQUARE_BRACKETS;
+        return start == CLOSE_PARENTHESES || start == CLOSE_BRACES || start ==              CLOSE_SQUARE_BRACKETS;
     }
 
     public static boolean endsWithOpen(String line){
-        int length = line.length() -1;
+        int length = line.length() - 1;
         char end = line.charAt(length);
         return end == OPEN_PARENTHESES || end == OPEN_BRACES || end == OPEN_SQUARE_BRACKETS;
     }
@@ -49,7 +50,7 @@ public class Main {
     public static void validateChar(char aux){
         if(aux == OPEN_PARENTHESES || aux == CLOSE_PARENTHESES) validateParentheses(aux);
         else if(aux == OPEN_BRACES || aux == CLOSE_BRACES) validateBraces(aux);
-        else if(aux == OPEN_SQUARE_BRACKETS || aux == CLOSE_SQUARE_BRACKETS) validateSquareBrackets(aux);
+        else if(aux == OPEN_SQUARE_BRACKETS || aux == CLOSE_SQUARE_BRACKETS)                 validateSquareBrackets(aux);
     }
 
     private static void validateParentheses(char aux) {
